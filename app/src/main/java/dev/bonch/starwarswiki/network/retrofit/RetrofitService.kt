b6fab77adadd7c2b7d8fe31f09307041f2a3ce16
Sery0077 <52316851+Sery0077@users.noreply.github.com>
2020-01-10 17:51:58 +0300

    Add search bar in PeopleFragment, FilmFragment, string resources. Change theme.
    Add string resources
    
    Add string resources for network error, nothing found for search people, film.

--- app/src/main/java/dev/bonch/starwarswiki/network/retrofit/RetrofitService.kt
+++ app/src/main/java/dev/bonch/starwarswiki/network/retrofit/RetrofitService.kt
@@ -4,6 +4,7 @@ import dev.bonch.starwarswiki.models.Film
 import dev.bonch.starwarswiki.models.People
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RetrofitService {
 
@@ -12,4 +13,10 @@ interface RetrofitService {
 
     @GET("films/")
     suspend fun getFilmsList(): Response<Film.Pojo>
+
+    @GET("people/")
+    suspend fun searchPeople(@Query("search") name: String): Response<People.Pojo>
+
+    @GET("films/")
+    suspend fun searchFilm(@Query("search") title: String): Response<Film.Pojo>
 }
